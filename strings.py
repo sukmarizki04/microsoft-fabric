fe361cd7f0777df5445d6f747d07f4a31409fcb8
sukmarizki04 <sukmarizki04@gmail.com>
2026-06-28 23:17:14 +0700

    Full the example string

--- strings.py
+++ strings.py
@@ -24,5 +24,38 @@ egg = 'egg'
 concatenates + egg
 
 # Formatting string 
+# Memformat panjang string
 name = "jhony"
 print("Hello, %s!" % name)
+
+name = "Jhony"
+age = 10
+print("%s is %d year old." % (name, age))
+
+txt1 = "My name is {fname}, I'm {age}".format(fname = "John", age= 36)
+txt1 = "My name is {0}, I'm {1}".format("John", 36)
+txt1 = "My name is {}, I'm {}".format("John",36)
+
+# Slicing string in python
+slicing = "they have one best motivated"
+print(slicing[2:5])
+print(slicing[0:2])
+print(slicing[:2])
+print(slicing[2:])
+print(slicing[:2] + slicing[:2])
+print(slicing[:])
+print(slicing[-5:-2])
+print(slicing[2:6])
+
+myNumber = '12124314513' * 10
+print(myNumber)
+print(type(myNumber))
+#Input something else
+name1 = input('my name....')
+# Join for the string 
+"""
+>>> "#".join(["John", "Peter", "Vicky"])
+'John#Peter#Vicky'
+"""
+word = "Hello, world!".endswith("!")
+print(word)
