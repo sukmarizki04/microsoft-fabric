1f91b2c8a9218d312a4999113eaa430c6391977c
sukmarizki04 <sukmarizki04@gmail.com>
2026-07-11 20:43:42 +0700

    pythonFstrings

---
+++ pythonFstrings.py
@@ -0,0 +1,13 @@
+#Fstring Usage to Develoment 
+website = "sukmarizki.com"
+print(f"Hello {website}")
+num = 20
+print(f"Nilai matematika {num}")
+print(f"He said  {"I am A Jhon"}")
+name =  "usman"
+age = 40
+print(f"Hello my name is {name}, Iam {age} years old")
+
+# "Using python fstrings Fill Align"
+print(f'{"text": 10}')
+print(f'{"test":*>10}')
